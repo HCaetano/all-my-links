677ce88b34322ea3461ee8f45b2378c8b938262c
Havyner Caetano <hrcgeo@gmail.com>
2024-07-11 11:52:05 -0300

    feat: Implement rendering list of links via json file

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementsByClassName("content__image")[0].src = data.avatar;
       document.getElementsByClassName("content__name")[0].textContent = data.name;
       document.getElementsByClassName("content__description")[0].textContent = data.description;
+
+      // render links block
+      const linksList = document.getElementsByClassName("content__link-container")[0];
+      linksList.innerHTML = "";
+
+      data.links.forEach((link) => {
+        const line = document.createElement("li");
+        linksList.appendChild(line);
+
+        const listItem = document.createElement("a");
+        listItem.className = "content__link";
+        listItem.href = link.url;
+
+        const itemText = document.createElement("p");
+        itemText.className = "content__link-text";
+        itemText.textContent = link.name;
+
+        listItem.appendChild(itemText);
+        line.appendChild(listItem);
+      });
     })
-    .catch((error) => console.error("Error fetching JSON data:", error));
+    .catch((error) => console.error("Error fetching content from JSON", error));
 });
